1bfcba86309541bc6b7f39738da3cf54c1ed29a1
otarza <otar.zakalashvili@gmail.com>
2023-02-28 07:31:51 +0400

    Fix logo image dimensions.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export function Logo(props) {
   return (
-    <Image src="https://www.girchi.com/themes/custom/girchi/dist/css/images/logo.png" alt="Logo" style={{maxHeight: '35px !important'}} />
+    <Image width={30} height={36} src="/logo.png" alt="Logo" style={{maxHeight: '35px !important'}} />
   )
 }
